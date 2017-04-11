3917b89537eaae37dfd75ad4455fec6e602c9eff
npolonskaya <natalya.polonskaya.repo@gmail.com>
2017-04-12 02:15:45 +0300

    Blank console project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GZipStreamMultithread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
